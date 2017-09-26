7901b480c43f94a6b554e96ffa9a2cddb0d9eb3e
Laura <l@veriny.tf>
2017-09-26 21:01:03 +0100

    Allow Python 3.5.

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 from sys import version_info
 
-ver = version_info[:2]
-if ver < (3, 6):
-    raise SystemExit('Sorry! multio requires python 3.6.0 or later.')
+ver = version_info[:3]
+if ver < (3, 5, 2):
+    raise SystemExit('Sorry! multio requires python 3.5.2 or later.')
 
 setup(
     name='multio',
     description='mulio - an unified async library for curio and trio',
     license='MIT',
-    version='0.0.1',
+    version='0.0.2',
     author='Mark Jameson - aka theelous3, and Akuli',
     url='https://github.com/theelous3/multio',
     packages=['multio'],
