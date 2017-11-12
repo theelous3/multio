17ed7a19b3057b88c2eeef072cd4403ff4d286b0
Laura F. D <l@veriny.tf>
2017-11-12 13:21:29 +0000

    Add the ability to register a handler for a specific library.
    This means the library isn't limited to only trio/curio; other things
    can extend it as needed.
    
    Additionally, added NotImplementedError to many library functions, for
    any registered extension that doesn't provide an implementation.

--- multio/__init__.py
+++ multio/__init__.py
@@ -2,6 +2,7 @@ import functools
 import inspect
 import sys
 import threading
+from typing import Callable
 
 
 async def _maybe_await(coro):
@@ -73,9 +74,11 @@ class SocketWrapper:
         '''
         Wraps a connection opening method in this class.
         '''
+
         async def inner(*args, **kwargs):
             sock = await meth(*args, **kwargs)
             return cls(sock)
+
         return inner
 
 
@@ -83,6 +86,7 @@ class Lock:
     '''
     Represents a lock.
     '''
+
     def __init__(self):
         self.lock = asynclib.Lock()
 
@@ -109,6 +113,7 @@ class Event:
     '''
     Represents an event.
     '''
+
     def __init__(self):
         self.event = asynclib.Event()
 
@@ -133,6 +138,7 @@ class Event:
         '''
         return self.event.clear()
 
+
 # So, the idea here is that multio, after import, must be told explicitly which
 # event loop to use. Upon first import we has _AsyncLib which is an empty
 # shell. Upon initialisation the instance of _AsyncLib multio uses is
@@ -150,57 +156,91 @@ class Event:
 # libraries themselves. Both are majestic and beautiful.
 
 
+class _AsyncLibManager:
+    '''
+    A manager for multio. This allows registering a library and a function that sets the 
+    attributes of the object for the appropriate library.
+    '''
+
+    def __init__(self):
+        self._handlers = {}
+
+    def register(self, library: str, cbl: Callable[['_AsyncLib'], None]):
+        '''
+        Registers a callable to set up a library.
+        '''
+        self._handlers[library] = cbl
+
+    def init(self, library: str, lib: '_AsyncLib'):
+        return self._handlers[library](lib)
+
+
+def _not_impl_generic(*args, **kwargs):
+    raise NotImplementedError
+
+
 class _AsyncLib(threading.local):
-    '''
-    When asynclib.something is requested, asynclib.__dict__['something']
-    is checked before asynclib.__getattr__('something')
-    '''
     _init = False
+
+    Lock = _not_impl_generic
+    Queue = _not_impl_generic
+    Cancelled = _not_impl_generic
+    Event = _not_impl_generic
+    TaskTimeout = _not_impl_generic
 
     async def aopen(self, *args, **kwargs):
         '''
         Opens an async file.
         '''
+        raise NotImplementedError
 
     async def open_connection(self, host: str, port: int, *args, **kwargs) -> SocketWrapper:
         '''
         Opens a connection. Returns a SocketWrapper.
         '''
+        raise NotImplementedError
 
     async def sleep(self, amount: float):
         '''
         Sleeps for a certain time.
         '''
+        raise NotImplementedError
 
     def task_manager(self, *args, **kwargs):
         '''
         Gets a task manager instance.
         '''
+        raise NotImplementedError
 
     async def spawn(self, taskgroup_or_nursery, coro, *args):
         '''
         Spawns a task in a taskgroup or nursery.
         '''
+        raise NotImplementedError
 
     def timeout_after(self, *args, **kwargs):
         '''
         Timeouts an operation after a certain period.
         '''
+        raise NotImplementedError
 
     async def sendall(self, sock, *args, **kwargs):
         '''
         Sends all data through a socket.
         '''
+        raise NotImplementedError
 
     async def recv(self, sock, *args, **kwargs) -> bytes:
         '''
         Receives data from a socket.
         '''
+        raise NotImplementedError
 
     async def sock_close(self, sock):
         '''
         Closes a socket.
         '''
+        raise NotImplementedError
 
     def __getattribute__(self, item):
         if super().__getattribute__("_init") is False:
@@ -209,11 +249,70 @@ class _AsyncLib(threading.local):
         return super().__getattribute__(item)
 
 
-# Singleton instance.
+# Singleton instances.
+manager = _AsyncLibManager()
 asynclib = _AsyncLib()
 
 
-def init(lib_name):
+def _curio_init(lib: _AsyncLib):
+    import curio
+    from ._event_loop_wrappers import (curio_sendall,
+                                       curio_recv,
+                                       curio_close,
+                                       curio_spawn)
+    lib.aopen = curio.aopen
+    lib.open_connection = curio.open_connection
+    lib.sleep = curio.sleep
+    lib.task_manager = curio.TaskGroup
+    lib.timeout_after = curio.timeout_after
+    lib.sendall = curio_sendall
+    lib.recv = curio_recv
+    lib.sock_close = curio_close
+    lib.spawn = curio_spawn
+
+    lib.Lock = curio.Lock
+    lib.Queue = curio.Queue
+    lib.Event = curio.Event
+    lib.Cancelled = curio.CancelledError
+    lib.TaskTimeout = curio.TaskTimeout
+
+
+def _trio_init(lib: _AsyncLib):
+    import trio
+    from ._event_loop_wrappers import (trio_open_connection,
+                                       trio_send_all,
+                                       trio_receive_some,
+                                       trio_close,
+                                       trio_spawn)
+    lib.aopen = trio.open_file
+    lib.sleep = trio.sleep
+    lib.task_manager = trio.open_nursery
+    lib.timeout_after = AsyncWithWrapper.wrap(trio.fail_after)
+    lib.open_connection = trio_open_connection
+    lib.sendall = trio_send_all
+    lib.recv = trio_receive_some
+    lib.sock_close = trio_close
+    lib.spawn = trio_spawn
+
+    lib.Lock = trio.Lock
+    lib.Queue = trio.Queue
+    lib.Cancelled = trio.Cancelled
+    lib.Event = trio.Event
+    lib.TaskTimeout = trio.TooSlowError
+
+
+manager.register("curio", _curio_init)
+manager.register("trio", _trio_init)
+
+
+def register(lib_name: str, cbl: Callable[[_AsyncLib], None]):
+    '''
+    Registers a new library function with the current manager.
+    '''
+    return manager.register(lib_name, cbl)
+
+
+def init(lib_name: str):
     '''
     Must be called at some point after import and before your event loop
     is run.
@@ -224,54 +323,7 @@ def init(lib_name):
     Args:
         lib_name (str): Either 'curio' or 'trio'.
     '''
-    if lib_name == 'curio':
-        import curio
-        from ._event_loop_wrappers import (curio_sendall,
-                                           curio_recv,
-                                           curio_close,
-                                           curio_spawn)
-        asynclib.aopen = curio.aopen
-        asynclib.open_connection = curio.open_connection
-        asynclib.sleep = curio.sleep
-        asynclib.task_manager = curio.TaskGroup
-        asynclib.timeout_after = curio.timeout_after
-        asynclib.sendall = curio_sendall
-        asynclib.recv = curio_recv
-        asynclib.sock_close = curio_close
-        asynclib.spawn = curio_spawn
-
-        asynclib.Lock = curio.Lock
-        asynclib.Queue = curio.Queue
-        asynclib.Event = curio.Event
-        asynclib.Cancelled = curio.CancelledError
-        asynclib.TaskTimeout = curio.TaskTimeout
-
-    elif lib_name == 'trio':
-        import trio
-        from ._event_loop_wrappers import (trio_open_connection,
-                                           trio_send_all,
-                                           trio_receive_some,
-                                           trio_close,
-                                           trio_spawn)
-        asynclib.aopen = trio.open_file
-        asynclib.sleep = trio.sleep
-        asynclib.task_manager = trio.open_nursery
-        asynclib.timeout_after = AsyncWithWrapper.wrap(trio.fail_after)
-        asynclib.open_connection = trio_open_connection
-        asynclib.sendall = trio_send_all
-        asynclib.recv = trio_receive_some
-        asynclib.sock_close = trio_close
-        asynclib.spawn = trio_spawn
-
-        asynclib.Lock = trio.Lock
-        asynclib.Queue = trio.Queue
-        asynclib.Cancelled = trio.Cancelled
-        asynclib.Event = trio.Event
-        asynclib.TaskTimeout = trio.TooSlowError
-
-    else:
-        raise RuntimeError('{} is not a supported library.'.format(lib_name))
-
+    manager.init(lib_name, asynclib)
     asynclib.lib_name = lib_name
     asynclib._init = True
 
@@ -298,6 +350,7 @@ class _ModWrapper:
     '''
     A wrapper that allows ``multio.<name>`` to proxy through to ``asynclib.<name>``.
     '''
+
     def __init__(self, mod):
         self.mod = mod
 
@@ -306,9 +359,9 @@ class _ModWrapper:
 
     def __getattr__(self, item):
         try:
-            return getattr(asynclib, item)
-        except AttributeError:
             return getattr(self.mod, item)
+        except AttributeError:
+            return getattr(asynclib, item)
 
 
 sys.modules[__name__] = _ModWrapper(sys.modules[__name__])
