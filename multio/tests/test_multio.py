c7ae3ba31fa6b97ba700828207a3f90a5360146a
Panos Mavrogiorgos <pmav99@gmail.com>
2018-02-14 20:35:24 +0200

    [tests] Simplify test structure
    No need to group tests in classes yet.

--- multio/tests/test_multio.py
+++ multio/tests/test_multio.py
@@ -6,8 +6,8 @@ import trio
 from .. import init
 
 
-class TestCase(object):
+@pytest.mark.parametrize("lib", ["trio", "curio", trio, curio])
+def test_initialize(lib):
+    init(lib)
 
-    @pytest.mark.parametrize("lib", ["trio", "curio", trio, curio])
-    def test_initialize(self, lib):
         init(lib)
