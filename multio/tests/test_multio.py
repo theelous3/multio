1a40b592a881f7f90d6773a262b4f574bcdc884e
Panos Mavrogiorgos <pmav99@gmail.com>
2018-02-14 20:36:06 +0200

    [tests] make sure that "curio" and "trio" are registered libraries.

--- multio/tests/test_multio.py
+++ multio/tests/test_multio.py
@@ -3,11 +3,15 @@ import pytest
 import curio
 import trio
 
-from .. import init
+from .. import init, manager
 
 
 @pytest.mark.parametrize("lib", ["trio", "curio", trio, curio])
 def test_initialize(lib):
     init(lib)
 
+
+@pytest.mark.parametrize("lib", ["curio", "trio"])
+def test_lib_is_registered(lib):
+    assert lib in manager._handlers
         init(lib)
