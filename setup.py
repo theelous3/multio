1e9e281d59b8736172529db2e8a929a677b4a6d4
theelous3 <thee_grandmaster@gmail.com>
2018-05-21 22:27:11 +0100

    dropped min py ver requirement from 3.6.x back to 3.5.2

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 from sys import version_info
 
 ver = version_info[:3]
-if ver < (3, 6, 0):
-    raise SystemExit('Sorry! multio requires python 3.6.0 or later.')
+if ver < (3, 5, 2):
+    raise SystemExit('Sorry! multio requires python 3.5.2 or later.')
 
 setup(
     name='multio',
     description='multio - an unified async library for curio and trio',
     license='MIT',
-    version='0.2.2',
+    version='0.2.3',
     author='theelous3, SunDwarf, and Akuli',
     url='https://github.com/theelous3/multio',
     python_requires=">=3.5.2",
